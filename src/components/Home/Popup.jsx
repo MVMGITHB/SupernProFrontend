0471959a7c6317b9bbde3ae08f189806ce1b7b03
himanshumvm <himanshusharma.mvmbs@gmail.com>
2025-06-05 13:23:57 +0530

    Popup increased

--- src/components/Home/Popup.jsx
+++ src/components/Home/Popup.jsx
@@ -19,23 +19,26 @@ export default function Popup() {
   if (!visible) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50  backdrop-blur-sm px-2">
-      <div className="relative max-w-sm w-full md:max-w-lg rounded-2xl overflow-hidden shadow-2xl bg-white">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm p-2">
+      <div className="relative w-full max-w-[95%] sm:max-w-[600px] md:max-w-3xl lg:max-w-5xl rounded-2xl overflow-hidden">
         {showClose && (
           <button
             onClick={() => setVisible(false)}
-            className="absolute top-2 right-2 z-10 bg-white/80 hover:bg-white rounded-full p-1 transition"
+            className="absolute top-2 right-2 lg:right-25 z-10 rounded-full p-1 transition"
           >
-            <X size={22} className="text-red-500 cursor-pointer" />
+            <X size={22} className="text-red-300 cursor-pointer" />
           </button>
         )}
-      <Link href="https://spectrum.gotrackier.com/click?campaign_id=1201&pub_id=945&source=%7B1%7D" target="_blank">
-        <img
-          src="/Pop up.webp"
-          alt="Popup"
-          className="w-full h-auto max-h-[90vh] object-cover"
-        />
-      </Link>
+        <Link
+          href="https://spectrum.gotrackier.com/click?campaign_id=1201&pub_id=945&source=%7B1%7D"
+          target="_blank"
+        >
+          <img
+            src="/Pop up.webp"
+            alt="Popup"
+            className="w-full h-auto max-h-[60vh] object-contain rounded-2xl"
+          />
+        </Link>
       </div>
     </div>
   );
