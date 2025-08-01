7cdb57c9a7b01380e8f5296ec37a1463cf9ae5a4
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-08-01 17:55:06 +0530

    tag remove

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,18 +46,7 @@ export default function RootLayout({ children }) {
         />
 
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17218636408"></script>
 
-        <script
-  dangerouslySetInnerHTML={{
-    __html: `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'AW-17218636408');
-    `,
-  }}
-/>
 
 
          <style>
